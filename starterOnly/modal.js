eaaab6e3b4928784ef4de78387db26bde2be5a12
remisany <remi.d.sany@gmail.com>
2021-04-19 10:03:12 +0200

    Vérification Prénom et Nom OK

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,52 +29,109 @@ cross.addEventListener("click", function(){
 });
 
 // Form
-
 const form = document.querySelector(".btn-submit");
 
+// Prénom & Nom
+const reg = /[~`!#$%\^&*+=\-\[\]\';,/{}|\":<>\?]/g;
+
+// Prénom
 const first = document.querySelector("#first");
 const firstlabel = document.querySelector("#firstlabel");
+
+// Nom
 const last = document.querySelector("#last");
 const lastlabel = document.querySelector("#lastlabel");
-const email = document.querySelector("#email");
-const emaillabel = document.querySelector("#emaillabel");
 
 function reset () {
+  //Reset prénom
   if (document.querySelector("#firstlabel span") != undefined) {
     firstlabel.removeChild(flspan);
   }
+
+  //Reset Nom
   if (document.querySelector("#lastlabel span") != undefined) {
     lastlabel.removeChild(llspan);
   }
-  if (document.querySelector("#emaillabel span") != undefined) {
-    emaillabel.removeChild(elspan);
-  }
 }
+
 
 form.addEventListener("click", function(event){
   event.preventDefault();
 
   reset();
+  
+  var flag = 0;
 
-  if (first.value == "") {
-    flspan = firstlabel.appendChild(document.createElement("span"));
-    flspan.innerHTML=" Le champ est obligatoire !";
-  } else if (first.value.length < 2) {
-    flspan = firstlabel.appendChild(document.createElement("span"));
+  //Vérifcation prénom
+  flspan = firstlabel.appendChild(document.createElement("span"));
+
+  const firstvalue = first.value;
+  const firstlength = firstvalue.length;
+  const firstArray = firstvalue.split("");
+
+  if (firstlength >= 2) {
+    for (i=0; i<firstlength; i++) {
+      if (isNaN(parseInt(firstArray[i])) == false) {
+        flspan.innerHTML=" Le champ ne doit contenir aucun chiffre !";
+        event.preventDefault();
+        flag = 1;
+      }
+
+      if (reg.test(firstArray[i])) {
+        flspan.innerHTML=" Le champ contient un caractère non accepté !";
+        event.preventDefault();
+        flag = 1;
+      }
+    }
+
+    if (flag == 0) {
+      flspan.classList.add("correct");
+      flspan.innerHTML=" Le champ est correct !";
+      flag = 0;
+    }
+
+  } else if (firstlength != 0) {
     flspan.innerHTML=" Le champ est trop court !";
-  } else if (isNaN(first.value) == false) {
-    flspan = firstlabel.appendChild(document.createElement("span"));
-    flspan.innerHTML=" Le champ ne doit contenir aucun chiffre !";
+    event.preventDefault();
   } else {
-    flspan = firstlabel.appendChild(document.createElement("span"));
-    flspan.classList.add("correct");
-    flspan.innerHTML=" Le champ est correct !";
+    flspan.innerHTML=" Le champ est obligatoire !";
+    event.preventDefault();
   }
 
-  ;
+  //Vérification nom
+  llspan = lastlabel.appendChild(document.createElement("span"));
 
+  const lastvalue = last.value;
+  const lastlength = lastvalue.length;
+  const lastArray = lastvalue.split("");
+
+  if (lastlength >= 2) {
+    for (i=0; i<lastlength; i++) {
+      if (isNaN(parseInt(lastArray[i])) == false) {
+        llspan.innerHTML=" Le champ ne doit contenir aucun chiffre !";
+        event.preventDefault();
+        flag = 1;
+      }
+
+      if (reg.test(lastArray[i])) {
+        llspan.innerHTML=" Le champ contient un caractère non accepté !";
+        event.preventDefault();
+        flag = 1;
+      }
+    }
+
+    if (flag == 0) {
+      llspan.classList.add("correct");
+      llspan.innerHTML=" Le champ est correct !";
+      flag = 0;
+    }
+
+  } else if (lastlength != 0) {
+    llspan.innerHTML=" Le champ est trop court !";
     event.preventDefault();
+  } else {
+    llspan.innerHTML=" Le champ est obligatoire !";
+    event.preventDefault();
+  }
 
-
-  
 });
