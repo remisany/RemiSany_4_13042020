66cceabfe0b2e0c26c81cd673573efea4e5d4fa9
remisany <remi.d.sany@gmail.com>
2021-04-20 17:30:29 +0200

    MAJ Design

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -67,193 +67,122 @@ var beforesubmit = 0;
 //First name & Last Name
 const regname = /[\\\^\$\*\+\?\.\(\)\|\[\]\{\}\-&~"#'`_@=¨£%µ,;/:§!]/g;
 
-//First Name
-const first = document.querySelector("#first");
-const firstlabel = document.querySelector("#firstlabel");
-
-//Last name
-const last = document.querySelector("#last");
-const lastlabel = document.querySelector("#lastlabel");
-
 //Email
-const email = document.querySelector("#email");
-const emaillabel = document.querySelector("#emaillabel");
 const regemail = /[a-z0-9_\-\.]+@[a-z0-9_\-\.]+\.[a-z]+/g;
-
-//Birthdate
-const birth = document.querySelector("#birthdate");
-const birthlabel = document.querySelector("#birthlabel");
-
-//Quantity
-const quantity = document.querySelector("#quantity");
-const quantitylabel = document.querySelector("#quantitylabel");
-
-//Radios
-const radioslabel = document.querySelector("#radioslabel");
-
-//Checkbox 1
-const checkbox1 = document.querySelector("#checkbox1");
-const checkbox1label = document.querySelector("#checkbox1label");
 
 //Reset
 function reset () {
   flag = 0;
   beforesubmit = 0;
 
-  //Reset First name
-  if (document.querySelector("#firstlabel span") != undefined) {
-    firstlabel.removeChild(flspan);
-  }
+  for (i=0; i<formData.length; i++) {
+      inputs[i].classList.remove("correct");
+      inputs[i].classList.remove("incorrect");
 
-  //Reset Last name
-  if (document.querySelector("#lastlabel span") != undefined) {
-    lastlabel.removeChild(llspan);
-  }
-
-  //Reset Birthdate
-  if (document.querySelector("#emaillabel span") != undefined) {
-    emaillabel.removeChild(elspan);
-  }
-
-  //Reset Birthdate
-  if (document.querySelector("#birthlabel span") != undefined) {
-    birthlabel.removeChild(blspan);
-  }
-
-  //Reset Quantity
-  if (document.querySelector("#quantitylabel span") != undefined) {
-    quantitylabel.removeChild(qlspan);
-  }
-
-  //Reset Radios
-  if (document.querySelector("#radioslabel span") != undefined) {
-    radioslabel.removeChild(rlspan);
-  }
-
-  //Reset Checkbox 1
-  if (document.querySelector("#checkbox1label #clspan") != undefined) {
-    checkbox1label.removeChild(clspan);
+    if (formData[i].lastElementChild.nodeName == "DIV") {
+      formData[i].removeChild(formData[i].lastChild);
+    }
   }
 };
 
 form.addEventListener("click", function(event){
   reset();
 
-  //First name verification
-  flspan = firstlabel.appendChild(document.createElement("span"));
+  //First name & Last name verification
+  for (i=0; i<2; i++) {
 
-  const firstvalue = first.value;
-  const firstlength = firstvalue.length;
-  const firstArray = firstvalue.split("");
+    div = document.createElement("div");
+    //div.setAttribute("class", "div");
+    div = formData[i].appendChild(div);
 
-  if (firstlength >= 2) {
-    for (i=0; i<firstlength; i++) {
-      if (isNaN(parseInt(firstArray[i])) == false) {
-        flspan.innerHTML=" Le champ ne doit contenir aucun chiffre !";
-        flag = 1;
+    const namevalue = inputs[i].value;
+    const namelength = namevalue.length;
+    const nameArray = namevalue.split("");
+  
+    if (namelength >= 2) {
+      for (let i=0; i<namelength; i++) {
+        if (isNaN(parseInt(nameArray[i])) == false) {
+          div.innerHTML=" Le champ ne doit contenir aucun chiffre !";
+          inputs[i].classList.add("incorrect");
+          flag = 1;
+        }
+  
+        if (regname.test(nameArray[i])) {
+          div.innerHTML=" Le champ contient un caractère non accepté !";
+          inputs[i].classList.add("incorrect");
+          flag = 1;
+        }
       }
-
-      if (regname.test(firstArray[i])) {
-        flspan.innerHTML=" Le champ contient un caractère non accepté !";
-        flag = 1;
+  
+      if (flag == 0) {
+        inputs[i].classList.add("correct");
+        console.log("oui")
+        flag = 0;
+        beforesubmit++;
       }
+  
+    } else if (namelength != 0) {
+      div.innerHTML=" Le champ est trop court !";
+      inputs[i].classList.add("incorrect");
+    } else {
+      div.innerHTML=" Le champ est obligatoire !";
+      inputs[i].classList.add("incorrect");
     }
-
-    if (flag == 0) {
-      flspan.classList.add("correct");
-      flspan.innerHTML=" Le champ est correct";
-      flag = 0;
-      beforesubmit++;
-    }
-
-  } else if (firstlength != 0) {
-    flspan.innerHTML=" Le champ est trop court !";
-  } else {
-    flspan.innerHTML=" Le champ est obligatoire !";
   }
-
-  //Last name verification
-  llspan = lastlabel.appendChild(document.createElement("span"));
-
-  const lastvalue = last.value;
-  const lastlength = lastvalue.length;
-  const lastArray = lastvalue.split("");
-
-  if (lastlength >= 2) {
-    for (i=0; i<lastlength; i++) {
-      if (isNaN(parseInt(lastArray[i])) == false) {
-        llspan.innerHTML=" Le champ ne doit contenir aucun chiffre !";
-        flag = 1;
-      }
-
-      if (regname.test(lastArray[i])) {
-        llspan.innerHTML=" Le champ contient un caractère non accepté !";
-        flag = 1;
-      }
-    }
-
-    if (flag == 0) {
-      llspan.classList.add("correct");
-      llspan.innerHTML=" Le champ est correct";
-      flag = 0;
-      beforesubmit++;
-    }
-
-  } else if (lastlength != 0) {
-    llspan.innerHTML=" Le champ est trop court !";
-  } else {
-    llspan.innerHTML=" Le champ est obligatoire !";
-  }
-
+  
   //Email verification
-  elspan = emaillabel.appendChild(document.createElement("span"));
+  div = document.createElement("div");
+  div = formData[2].appendChild(div);
 
-  const emailvalue = email.value;
+  const emailvalue = inputs[2].value;
   const emaillength = emailvalue.length;
 
   if (emaillength != 0) {
     if (emailvalue.match(regemail)) {
-      elspan.classList.add("correct");
-      elspan.innerHTML=" Le champ est correct";
+      inputs[2].classList.add("correct");
       beforesubmit++;
     } else {
-      elspan.innerHTML=" L'adresse mail est invalide !";
+      div.innerHTML=" L'adresse mail est invalide !";
+      inputs[2].classList.add("incorrect");
     }
   } else {
-    elspan.innerHTML=" Le champ est obligatoire !";
+    div.innerHTML=" Le champ est obligatoire !";
+    inputs[2].classList.add("incorrect");
   }
 
   //Birthdate verification
-  blspan = birthlabel.appendChild(document.createElement("span"));
+  div = document.createElement("div");
+  div = formData[3].appendChild(div);
 
-  const birthvalue = birth.value;
+  const birthvalue = inputs[3].value;
   const birthlength = birthvalue.length;
   
   if (birthlength == 0) {
-    blspan.innerHTML=" Le champ est obligatoire !";
+    div.innerHTML=" Le champ est obligatoire !";
   } else {
-    blspan.classList.add("correct");
-    blspan.innerHTML=" Le champ est correct";
+    inputs[3].classList.add("correct");
     beforesubmit++;
   }
 
   //Quantity verification
-  qlspan = quantitylabel.appendChild(document.createElement("span"));
+  div = document.createElement("div");
+  div = formData[4].appendChild(div);
 
-  const quantityvalue = quantity.value;
+  const quantityvalue = inputs[4].value;
   const quantitylength = quantityvalue.length;
   
   if (quantitylength == 0) {
-    qlspan.innerHTML=" Le champ est obligatoire !";
+    div.innerHTML=" Le champ est obligatoire !";
   } else {
-    qlspan.classList.add("correct");
-    qlspan.innerHTML=" Le champ est correct";
+    inputs[4].classList.add("correct");
     beforesubmit++;
   }
 
   //Location verification
   const radios = document.getElementsByName("location");
-  rlspan = radioslabel.appendChild(document.createElement("span"));
+
+  div = document.createElement("div");
+  div = formData[5].appendChild(div);
 
   const radioslength = radios.length;
   var cpt = 0;
@@ -265,25 +194,22 @@ form.addEventListener("click", function(event){
   }
 
   if ((cpt == quantityvalue) && (quantityvalue !== "")) {
-    rlspan.classList.add("correct");
-    rlspan.innerHTML=" Le champ est correct";
+    inputs[5].classList.add("correct");
     beforesubmit++;
   } else if ((quantityvalue == "") && (cpt == 0)){
-    rlspan.innerHTML="";
+    div.innerHTML="";
   } else {
-    rlspan.innerHTML=" Le nombre de ville selectionné est incorrect !";
+    div.innerHTML=" Le nombre de ville selectionné est incorrect !";
   }
 
   //Checkbox 1 verification
-  clspan = document.createElement("span");
-  clspan.setAttribute("id", "clspan");
-  clspan = checkbox1label.appendChild(clspan);
+  div = document.createElement("div");
+  div = formData[6].appendChild(div);
 
   if (checkbox1.checked == false) {
-    clspan.innerHTML=" L'acceptation est obligatoire !";
+    div.innerHTML=" L'acceptation des conditions d'utilisation est obligatoire !";
   } else {
-    clspan.classList.add("correct");
-    clspan.innerHTML=" Le champ est correct";
+    inputs[6].classList.add("correct");
     beforesubmit++;
   }
 
